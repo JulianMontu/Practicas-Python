278bba74a3133fa286f9281e0ef550864a96b93b
JulianMontu <julamontufar@misena.edu.co>
2022-06-06 23:14:23 -0500

    Iva,totales

--- Functions.py
+++ Functions.py
@@ -1,40 +1,54 @@
-#Function input code and name product
-def ingresoCodigo():
-    _codigo_ = input("Ingrese el codigo del producto")
-    _nombre_product = input("Ingrese el nombre del producto")
+def print_Tu_vecino():
     printLinea()
-    print("El producto ", _nombre_product, "Tiene el codigo ", _codigo_)
+    print("  Bienvenido a Tu Vecino ")
+    printLinea()
 
-#Function estimate subtotal of buy
-def subTotal(_cantidad_:float, _valorUnitario_:float):
+
+# Function input code and name product
+def datos():
+    cantidades=1;
+    cantidadProductos = int(input("Ingrese el numero de productos: "))
+    while cantidades <= cantidadProductos:
+        _codigo_ = input("Ingrese el codigo del producto: ")
+        _nombre_product = input("Ingrese el nombre del producto: ")
+        cantidad = float(input("Ingrese la cantidad del producto: "+_nombre_product))
+        _valorUnitario = float(input("Ingrese el valor unitario del producto: "))
+        ingresarDetalleProducto(_codigo_, _nombre_product, cantidad, _valorUnitario)
+        subTotal(cantidad, _valorUnitario)
+        cantidades+=1
+    print("Muchas gracias por su compra")
+
+
+def ingresarDetalleProducto(_codigo_, _nombreProduct,_cantidad,_valorUnitario):
+    printLinea()
+    print("Codigo producto: ", _codigo_, "\nDetalle Producto: ", _nombreProduct,"\t Cantidad: ",_cantidad,"\t Valor Unitario: ",_valorUnitario)
+
+
+
+
+# Function estimate subtotal of buy
+def subTotal(_cantidad_: float, _valorUnitario_: float):
     if (_cantidad_ and _valorUnitario_ <= 0):
         print("No se puede realizar la operacion")
     else:
         _sub_total = float(_valorUnitario_ * _cantidad_)
-        print("Cantidad ", _cantidad_)
-        printLinea()
-        print("Valor Unitario ", _valorUnitario_)
-        printLinea()
-        print("Subtotal ", _sub_total)
         totalConIva(_sub_total)
-#Function of total buy with total iva
-def totalConIva(total:float):
-    _valor_iva_ = float(0.19)
-    _siTiene = input("Tiene iva? si o no ")
-    if (_siTiene == 'si'):
-        _total_iva_ =(_valor_iva_ * total)  + total
-        printLinea()
-        print("Total Compra ", _total_iva_, " IVA", _valor_iva_ * 100, " %")
-        if (_total_iva_ > 100000):
-            descuentoHoy = 0.20
-            totalDescuento =   _total_iva_ - (_total_iva_ * descuentoHoy)
-            printLinea()
-            print("Tiene descuento del ", descuentoHoy * 100, "%")
-            printLinea()
-            print("Total con descuento ", totalDescuento)
-    elif (_siTiene == "no"):
-        printLinea()
-        print("Total compra ", total, " No tiene IVA")
-#print = how invoice
+
+
+# Function of total buy with total iva
+def totalConIva(total: float):
+    print("Escoga la tipo de iva\n Opcion 1: Excento de iva\n opcion 2:Bienes Iva 5%\nOpcion 3: General Iva 19%")
+    opcion=int(input("Ingrese el tipo de iva"))
+    if(opcion==1):
+        print("Subtotal: ",total,"\t Total: ",total,"\t Excento de iva")
+    elif(opcion==2):
+        iva=0.5
+        totalIva=(total*iva)+total
+        print("Subtotal: ", total, "\t Total: ", totalIva, "\t Iva 5%")
+    elif(opcion==3):
+        iva=0.19
+        totalIva=(total*iva)+total
+        print("Subtotal: ", total, "\t Total: ", totalIva, "\t Iva 19%")
+# print = how invoice
 def printLinea():
-    print("="*50)
+    print("=" * 50)
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #import modules
 import Functions as fc
-#function example
-def print_Tu_vecino():
-    fc.printLinea()
-    print("  Bienvenido a Tu Vecino ")
-    fc.printLinea()
-
 #Run main
 if __name__ == '__main__':
-    print_Tu_vecino()
-    fc.ingresoCodigo()
+    fc.print_Tu_vecino()
+    fc.datos()
     fc.printLinea()
-    cantidad = float(input("ingrese la cantidad del producto por unidad "))
+
     fc.printLinea()
-    valorUnitario = float(input("Ingrese el valor unitario del producto "))
-    fc.printLinea()
-    fc.subTotal(cantidad, valorUnitario)
-    fc.printLinea()
+
+
